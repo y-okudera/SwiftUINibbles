60f5ea206f44b3a7fb738d1e163651294c49edc3
yuoku <appledev.yuoku@gmail.com>
2021-10-17 01:57:31 +0900

    Create a new iOS project.

---
+++ Application/SwiftUINibbles_iOS/SwiftUINibbles_iOS/SwiftUINibbles_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINibbles_iOSApp.swift
+//  SwiftUINibbles_iOS
+//
+//  Created by Yuki Okudera on 2021/10/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINibbles_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
