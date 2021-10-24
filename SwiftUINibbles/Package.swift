4a67a1cf32bfc1510f07aa535f16ea2e4a8fb2da
yuoku <appledev.yuoku@gmail.com>
2021-10-24 19:58:32 +0900

    Rename directory from Assets to Resources.

--- SwiftUINibbles/Package.swift
+++ SwiftUINibbles/Package.swift
@@ -27,7 +27,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DesignSystem",
-            dependencies: ["Model"]),
+            dependencies: [
+                "Model",
+            ],
+            resources: [
+                .process("Resources")
+            ]
+        ),
         .target(
             name: "Model",
             dependencies: []),
